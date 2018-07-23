e7e3a20d928a60b28895b23a0481177289a550c2
Rakhitha <rakhitharr@live.com>
2018-07-23 16:51:45 +0530

    some tweaks

--- src/main/java/OAuthImplement/OAuthClient.java
+++ src/main/java/OAuthImplement/OAuthClient.java
@@ -4,12 +4,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class OAuthClient extends HttpServlet {
 
     String tokenEndpoint = "https://localhost:9443/oauth2/token";
+    String redirect_uri;
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         PrintWriter out = response.getWriter();
@@ -29,7 +31,7 @@ public class OAuthClient extends HttpServlet {
                             "<td>Authorization Code  </td><td>: <input type=\"text\" name=authCode value=" + request.getParameter("code") + " size=\"50\" readonly></td>\n"+
                         "</tr>\n"+
                         "<tr>" +
-                            "<td>Callback URI  </td><td colspan=\"4\">: <input type=\"text\" name=\"redirect_uri\" id=\"callbackURI\" size=\"50\" ></td>" +
+                            "<td>Callback URI  </td><td colspan=\"4\">: <input type=\"text\" name=\"redirect_uri\" id=\"redirect_uri\" value=" + redirect_uri + "size=\"50\" ></td>" +
                         "</tr>\n"+
                         "<tr>" +
                             "<td>Token Endpoint  </td><td colspan=\"4\">:   <input type=\"text\" name=\"tokenEndpoint\" id=\"tokenEndpoint\" value="+ tokenEndpoint+ " size=\"50\" readonly></td>" +
