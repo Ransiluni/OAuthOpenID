f72354b5f683b3053e50df94c280fb16913e4eae
Rakhitha <rakhitharr@live.com>
2018-07-24 15:04:17 +0530

    dynamic changes.

--- src/main/java/OAuthImplement/OAuthClient.java
+++ src/main/java/OAuthImplement/OAuthClient.java
@@ -10,8 +10,10 @@ import java.io.PrintWriter;
 
 public class OAuthClient extends HttpServlet {
 
-    String tokenEndpoint = "https://localhost:9443/oauth2/token";
+//    String tokenEndpoint = "https://localhost:9443/oauth2/token";
     String redirect_uri;
+    String grant_type = "authorization_code";
+    String client_id = "onKvXou89QW4m3aJRVLhtw4O8n4a";
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         PrintWriter out = response.getWriter();
@@ -27,10 +29,10 @@ public class OAuthClient extends HttpServlet {
             "</head>\n" +
             "<body>\n" +
                 "<h2>Getting Access Token and ID Token</h2>\n" +
-                "<form action=\"https://localhost:9443/oauth2/token\" method=\"post\" name=\"TokenRequest\" id=\"TokenRequest\">\n"+
+                "<form action=\"\" method=\"post\" name=\"TokenRequest\" id=\"TokenRequest\">\n"+
                     "<table class=\"user_pass_table\">\n"+
                         "<tr>" +
-                        "<td>Grant Type  </td><td>: <input type=\"text\" name=grant_type value=\"authorization_code\" size=\"50\" readonly></td>\n"+
+                        "<td><input type=\"text\" name=grant_type value=\"" + grant_type + "\" size=\"50\" readonly hidden></td>\n"+
                         "</tr>\n"+
                         "<tr>" +
                             "<td>Authorization Code  </td><td>: <input type=\"text\" name=code value=" + request.getParameter("code") + " size=\"50\" readonly></td>\n"+
@@ -38,19 +40,27 @@ public class OAuthClient extends HttpServlet {
                         "<tr>" +
                             "<td>Callback URI  </td><td colspan=\"4\">: <input type=\"text\" name=\"redirect_uri\" id=\"redirect_uri\" size=\"50\" ></td>" +
                         "</tr>\n"+
-//                        "<tr>" +
-//                            "<td>Token Endpoint  </td><td colspan=\"4\">:   <input type=\"text\" name=\"tokenEndpoint\" id=\"tokenEndpoint\" value="+ tokenEndpoint+ " size=\"50\" readonly></td>" +
-//                        "</tr>\n"+
+                        "<tr>" +
+                            "<td>Token Endpoint  </td><td colspan=\"4\">:   <input type=\"text\" name=\"tokenEndpoint\" id=\"tokenEndpoint\" size=\"50\" ></td>" +
+                        "</tr>\n"+
+                        "<tr>" +
+                            "<td>Client ID  </td><td>:  <input type=\"text\" name=\"client_id\" id=\"client_id\" value=\"" + client_id +"\"size=\"50\" readonly></td>" +
+                        "</tr>\n"+
                         "<tr>" +
                             "<td>Client Secret  </td><td>:  <input type=\"password\" name=\"client_secret\" id=\"client_secret\" size=\"50\" ></td>" +
                         "</tr>\n"+
                         "<tr>" +
-                            "<td colspan=\"2\"><input type=\"submit\" name=\"getTokens\" value=\"Get Tokens\"></td>" +
+                            "<td colspan=\"2\"><input type=\"submit\" name=\"getTokens\" value=\"Get Tokens\" onclick=\"changeEnd()\"></td>" +
                         "</tr>" +
                     "</table>" +
                 "</form>\n" +
-            "<script></script>"+
-            "</body>" +
+            "<script>\n" +
+                "function changeEnd(){" +
+                    "val = document.getElementById(\"tokenEndpoint\").value;" +
+                    "document.getElementById(\"TokenRequest\").action = val;" +
+                "}" +
+            "</script>\n"+
+            "</body>\n" +
             "</html>"
         );
     }
