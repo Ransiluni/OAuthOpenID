684e913053281daa843d765b9ff2c6ec2d7f0706
ransiluni_k <ransilunikannangara@gmail.com>
2018-08-01 14:24:53 +0530

    client

--- src/main/java/OAuthImplement/OAuthClient.java
+++ src/main/java/OAuthImplement/OAuthClient.java
@@ -29,44 +29,42 @@ public class OAuthClient extends HttpServlet {
             String docType =
                     "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
 
-            out.println(docType +
+
+
+            out.println(docType+
                     "<html>\n" +
                     "<head>" +
                     "<title>" + title + "</title>\n" +
                     "</head>\n" +
                     "<body>\n" +
-                        "<h2>Getting Access Token and ID Token</h2>\n" +
-                        "<form action=\"\" method=\"post\" name=\"TokenRequest\" id=\"TokenRequest\">\n"+
-                            "<table class=\"user_pass_table\">\n"+
-                                "<tr>" +
-                                    "<td><input type=\"text\" name=grant_type value=\"" + grant_type + "\" size=\"50\" readonly hidden></td>\n"+
-                                "</tr>\n"+
-                                "<tr>" +
-                                    "<td>Authorization Code  </td><td>: <input type=\"text\" name=code value=" + request.getParameter("code") + " size=\"50\" readonly></td>\n"+
-                                "</tr>\n"+
-                                "<tr>" +
-                                    "<td>Callback URI  </td><td colspan=\"4\">: <input type=\"text\" name=\"redirect_uri\" id=\"redirect_uri\" value=\"" + redirect_uri +"\" size=\"50\" ></td>" +
-                                "</tr>\n"+
-                                "<tr>" +
-                                    "<td>Token Endpoint  </td><td colspan=\"4\">:   <input type=\"text\" name=\"tokenEndpoint\" id=\"tokenEndpoint\" value=\"" + tokenEndpoint + "\" size=\"50\" ></td>" +
-                                "</tr>\n"+
-                                "<tr>" +
-                                    "<td>Client ID  </td><td>:  <input type=\"text\" name=\"client_id\" id=\"client_id\" value=\"" + client_id +"\"size=\"50\" readonly></td>" +
-                                "</tr>\n"+
-                                "<tr>" +
-                                    "<td>Client Secret  </td><td>:  <input type=\"password\" name=\"client_secret\" id=\"client_secret\" size=\"50\" ></td>" +
-                                "</tr>\n"+
-                                "<tr>" +
-                                    "<td colspan=\"2\"><input type=\"submit\" name=\"getTokens\" value=\"Get Tokens\" onclick=\"changeEnd()\"></td>" +
-                                "</tr>" +
-                            "</table>" +
-                        "</form>\n" +
-                    "<script>\n" +
-                    "function changeEnd(){" +
-                        "val = document.getElementById(\"tokenEndpoint\").value;" +
-                        "document.getElementById(\"TokenRequest\").action = val;" +
-                    "}" +
-                    "</script>\n"+
+                    "<h2>Getting Access Token and ID Token</h2>\n" +
+                    "<form action=\"JSON\" method=\"post\" name=\"TokenRequest\" id=\"TokenRequest\">\n"+
+                    "<form action=\"\" method=\"post\" name=\"TokenRequest\" id=\"TokenRequest\">\n"+
+                    "<table class=\"user_pass_table\">\n"+
+                    "<tr>" +
+                    "<td><input type=\"text\" name=grant_type value=\"" + grant_type + "\" size=\"50\" readonly hidden></td>\n"+
+                    "</tr>\n"+
+                    "<tr>" +
+                    "<td>Authorization Code  </td><td>: <input type=\"text\" name=code value=" + request.getParameter("code") + " size=\"50\" readonly></td>\n"+
+                    "</tr>\n"+
+                    "<tr>" +
+                    "<td>Callback URI  </td><td colspan=\"4\">: <input type=\"text\" name=\"redirect_uri\" id=\"redirect_uri\" value=\"" + redirect_uri +"\" size=\"50\" ></td>" +
+                    "</tr>\n"+
+                    "<tr>" +
+                    "<td>Token Endpoint  </td><td colspan=\"4\">:   <input type=\"text\" name=\"tokenEndpoint\" id=\"tokenEndpoint\" value=\"" + tokenEndpoint + "\" size=\"50\" ></td>" +
+                    "</tr>\n"+
+                    "<tr>" +
+                    "<td>Client ID  </td><td>:  <input type=\"text\" name=\"client_id\" id=\"client_id\" value=\"" + client_id +"\"size=\"50\" readonly></td>" +
+                    "</tr>\n"+
+                    "<tr>" +
+                    "<td>Client Secret  </td><td>:  <input type=\"password\" name=\"client_secret\" id=\"client_secret\" size=\"50\" ></td>" +
+                    "</tr>\n"+
+                    "<tr>" +
+                    "<td colspan=\"2\"><input type=\"submit\" name=\"getTokens\" value=\"Get Tokens\" onclick=\"changeEnd()\"></td>" +
+                    "</tr>" +
+                    "</table>" +
+                    "</form>\n" +
+
                     "</body>\n" +
                     "</html>"
             );
@@ -189,47 +187,7 @@ public class OAuthClient extends HttpServlet {
         }
 
 
-<<<<<<< HEAD
-        out.println(docType +
-            "<html>\n" +
-            "<head>" +
-                "<title>" + title + "</title>\n" +
-            "</head>\n" +
-            "<body>\n" +
-                "<h2>Getting Access Token and ID Token</h2>\n" +
-                "<form action=\"JSON\" method=\"post\" name=\"TokenRequest\" id=\"TokenRequest\">\n"+
-                "<form action=\"\" method=\"post\" name=\"TokenRequest\" id=\"TokenRequest\">\n"+
-                    "<table class=\"user_pass_table\">\n"+
-                        "<tr>" +
-                        "<td><input type=\"text\" name=grant_type value=\"" + grant_type + "\" size=\"50\" readonly hidden></td>\n"+
-                        "</tr>\n"+
-                        "<tr>" +
-                            "<td>Authorization Code  </td><td>: <input type=\"text\" name=code value=" + request.getParameter("code") + " size=\"50\" readonly></td>\n"+
-                        "</tr>\n"+
-                        "<tr>" +
-                            "<td>Callback URI  </td><td colspan=\"4\">: <input type=\"text\" name=\"redirect_uri\" id=\"redirect_uri\" value=\"" + redirect_uri +"\" size=\"50\" ></td>" +
-                        "</tr>\n"+
-                        "<tr>" +
-                            "<td>Token Endpoint  </td><td colspan=\"4\">:   <input type=\"text\" name=\"tokenEndpoint\" id=\"tokenEndpoint\" value=\"" + tokenEndpoint + "\" size=\"50\" ></td>" +
-                        "</tr>\n"+
-                        "<tr>" +
-                            "<td>Client ID  </td><td>:  <input type=\"text\" name=\"client_id\" id=\"client_id\" value=\"" + client_id +"\"size=\"50\" readonly></td>" +
-                        "</tr>\n"+
-                        "<tr>" +
-                            "<td>Client Secret  </td><td>:  <input type=\"password\" name=\"client_secret\" id=\"client_secret\" size=\"50\" ></td>" +
-                        "</tr>\n"+
-                        "<tr>" +
-                            "<td colspan=\"2\"><input type=\"submit\" name=\"getTokens\" value=\"Get Tokens\" onclick=\"changeEnd()\"></td>" +
-                        "</tr>" +
-                    "</table>" +
-                "</form>\n" +
-
-            "</body>\n" +
-            "</html>"
-        );
-=======
->>>>>>> 9b70ab8fa7c4c0095730de3a87d9e94fce65e34f
-    }
+ }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response){
 
