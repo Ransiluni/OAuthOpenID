4838f39d3720baa5ed9c2642f5f86f791ff158e2
Rakhitha <rakhitharr@live.com>
2018-08-14 12:04:04 +0530

    scope addition

--- src/main/java/OAuthImplement/Introspection.java
+++ src/main/java/OAuthImplement/Introspection.java
@@ -29,12 +29,10 @@ public class Introspection extends HttpServlet {
         user_name=request.getParameter("username");
         password=request.getParameter("password");
         System.out.println(user_name);
-        String url = " http://localhost:8080/ProtectedResource";
+        String url = " http://localhost:8081/ProtectedResource/ProtectedResource";
 
         if(user_name==null){
             access_Token = (String)session.getAttribute("access_token");
-//            request.setAttribute("Authorization", "Bearer " + access_Token);
-//            request.getRequestDispatcher("ProtectedResource").forward(request, response);
 
             URL object = new URL(url);
             HttpURLConnection con = (HttpURLConnection) object.openConnection();
@@ -73,7 +71,6 @@ public class Introspection extends HttpServlet {
 
             }
             catch (IOException e){
-                System.out.println("RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR: "+con.getResponseCode());
                 response.setContentType("application/json");
                 response.setStatus(401);
             }
@@ -81,8 +78,6 @@ public class Introspection extends HttpServlet {
         else{
             String encoding = Base64.getEncoder().encodeToString(
                     (user_name+":"+password).getBytes("utf-8"));
-//            request.setAttribute("Authorization", "Basic " + encoding);
-//            request.getRequestDispatcher("ProtectedResource").forward(request,response);
 
             URL object = new URL(url);
             HttpURLConnection con = (HttpURLConnection) object.openConnection();
@@ -105,9 +100,7 @@ public class Introspection extends HttpServlet {
 
                 //Read JSON response
                 JSONObject result = new JSONObject();
-//                System.out.println("output: "+re.toString());
                 org.json.JSONObject myResponse = new org.json.JSONObject(re.toString());
-//                System.out.println(myResponse);
                 Set<String> keys = myResponse.keySet();
                 for(String key : keys){
                     System.out.println(key);
@@ -128,43 +121,6 @@ public class Introspection extends HttpServlet {
             }
         }
 
-//        String url = "http://localhost:8080/ProtectedResource";
-//        URL obj = new URL(url);
-//
-//        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-//        // optional default is GET
-//        con.setRequestMethod("POST");
-//        //add request header
-//        con.setRequestProperty("Content-type", "application/x-www-form-urlencoded");
-//        con.setRequestProperty("Authorization", "Bearer " + access_Token);
-//
-//
-//
-////
-//
-//
-////        int responseCode = con.getResponseCode();
-////        System.out.println("\nSending 'GET' request to URL : " + url);
-////        System.out.println("Response Code : " + responseCode);
-//        try
-//
-//        {
-//            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-//            String inputLine;
-//            StringBuffer re = new StringBuffer();
-//            while ((inputLine = in.readLine()) != null) {
-//                re.append(inputLine);
-//            }
-//            in.close();
-//
-//            //Read JSON response and print
-//            //JSONObject myResponse = new JSONObject(re.toString());
-//            System.out.println(re.toString());
-////        System.out.println(myResponse);
-////        if (myResponse != null) {
-////            session.setAttribute("grant_type", "token");
-
-        //response.setContentType("text/html;charset=UTF-8");
     }
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request,response);
