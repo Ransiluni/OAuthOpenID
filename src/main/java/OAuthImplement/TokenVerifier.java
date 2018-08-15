511ebb1dcc7f929021191a780e61eb4197bb812a
Rakhitha <rakhitharr@live.com>
2018-08-15 16:13:43 +0530

    dynamic token validation

--- src/main/java/OAuthImplement/TokenVerifier.java
+++ src/main/java/OAuthImplement/TokenVerifier.java
@@ -5,25 +5,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
-import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.KeyFactory;
 import java.security.KeyStore;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.Certificate;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.RSAPublicKeySpec;
+import java.util.Set;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.nimbusds.jose.JWSVerifier;
 import com.nimbusds.jose.crypto.RSASSAVerifier;
 import com.nimbusds.jwt.SignedJWT;
 import org.apache.commons.codec.binary.Base64;
+import org.json.JSONArray;
+import org.json.simple.JSONObject;
 
 public class TokenVerifier extends HttpServlet {
 
@@ -37,8 +43,9 @@ public class TokenVerifier extends HttpServlet {
 
 
         String alias = "wso2carbon";
-        String token = request.getParameter("idToken");
+
         try {
+            String token = request.getParameter("idToken");
             String audience = (String) session.getAttribute("client_id");
             String grantType = (String) session.getAttribute("grant_type");
             String nonce = (String) session.getAttribute("nonce");
@@ -64,44 +71,83 @@ public class TokenVerifier extends HttpServlet {
             }
             String at_hash = new String(Base64.encodeBase64URLSafe(leftmost));
 //        System.out.println(at_hash);
-            try {
-                RSAPublicKey publicKey = null;
-                InputStream file = new FileInputStream("src/main/resources/wso2carbon.jks");
+            String kid;
+            String modulus;
+            String exponent;
+            try{
+                String url = "https://localhost:9443/oauth2/jwks";
+                URL object = new URL(url);
+                HttpURLConnection con = (HttpURLConnection) object.openConnection();
 
-                KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
-                keystore.load(file, alias.toCharArray());
+                con.setRequestMethod("GET");
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
-                Certificate cert = keystore.getCertificate(alias);
-                publicKey = (RSAPublicKey) cert.getPublicKey();
-
-                Algorithm alg = Algorithm.RSA256(publicKey, null);
-
-                if ("authorization_code".equals(grantType)) {
-                    JWTVerifier verifier = JWT.require(alg)
-                            .withIssuer("https://localhost:9443/oauth2/token")
-                            .withSubject("admin")
-                            .withAudience(audience)
-                            .withClaim("at_hash", at_hash)
-                            .build();
-
-                    DecodedJWT jwt = verifier.verify(token);
+                String inputLine;
+                StringBuffer re = new StringBuffer();
+                while ((inputLine = in.readLine()) != null) {
+                    re.append(inputLine);
                 }
-                if ("token".equals(grantType)) {
-                    JWTVerifier verifier = JWT.require(alg)
-                            .withIssuer("https://localhost:9443/oauth2/token")
-                            .withSubject("admin")
-                            .withAudience(audience)
-                            .withClaim("nonce", nonce)
-                            .withClaim("at_hash", at_hash)
-                            .build();
+                in.close();
 
-                    DecodedJWT jwt = verifier.verify(token);
+                org.json.JSONObject myResponse = new org.json.JSONObject(re.toString());
+                JSONArray myArray = myResponse.getJSONArray("keys");
+                kid = myArray.getJSONObject(0).getString("kid");
+                modulus = myArray.getJSONObject(0).getString("n");
+                exponent = myArray.getJSONObject(0).getString("e");
+
+                DecodedJWT decJWT = JWT.decode(token);
+                if(kid.equals(decJWT.getKeyId())){
+
+                    RSAPublicKey publicKey = null;
+//                InputStream file = new FileInputStream("src/main/resources/wso2carbon.jks");
+
+//                KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+//                keystore.load(file, alias.toCharArray());
+
+//                Certificate cert = keystore.getCertificate(alias);
+//                Certificate cert = "";
+//                publicKey = (RSAPublicKey) cert.getPublicKey();
+
+                    KeyFactory kf = KeyFactory.getInstance("RSA");
+                    BigInteger mod = new BigInteger(1,Base64.decodeBase64(modulus));
+                    BigInteger exp = new BigInteger(1,Base64.decodeBase64(exponent));
+
+                    publicKey = (RSAPublicKey) kf.generatePublic(new RSAPublicKeySpec(mod,exp));
+
+                    Algorithm alg = Algorithm.RSA256(publicKey, null);
+
+                    if ("authorization_code".equals(grantType)) {
+                        JWTVerifier verifier = JWT.require(alg)
+                                .withIssuer("https://localhost:9443/oauth2/token")
+                                .withSubject("admin")
+                                .withAudience(audience)
+                                .withClaim("at_hash", at_hash)
+                                .build();
+
+                        DecodedJWT jwt = verifier.verify(token);
+                    }
+                    if ("token".equals(grantType)) {
+                        JWTVerifier verifier = JWT.require(alg)
+                                .withIssuer("https://localhost:9443/oauth2/token")
+                                .withSubject("admin")
+                                .withAudience(audience)
+                                .withClaim("nonce", nonce)
+                                .withClaim("at_hash", at_hash)
+                                .build();
+
+                        DecodedJWT jwt = verifier.verify(token);
+                    }
+                    out.println(
+                            "<h2>\" Token Verified.  \"</title>" +
+                                    "</body>\n" +
+                                    "</html>"
+                    );
                 }
-                out.println(
-                        "<h2>\" Token Verified.  \"</title>" +
-                                "</body>\n" +
-                                "</html>"
-                );
+                else{
+                    throw new IOException();
+                }
+
+
 
 
 //            Use this if nimbusds is used.
@@ -129,7 +175,6 @@ public class TokenVerifier extends HttpServlet {
                                 "</html>"
                 );
             }
-
 
         } catch (Exception e) {
             response.sendRedirect("home?errorMessage=ID Token Error");
